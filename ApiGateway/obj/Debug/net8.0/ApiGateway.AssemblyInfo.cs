fa4bd57e24c7504ab0c40fb96a8ef4d34388f59c
Antenor1998 <AntenorE18@hotmail.com>
2024-11-06 20:19:25 -0600

    Add ocelot

--- ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5115a307fbad9076e6468105e34fac68424f492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9650fa7959c26846c026728d6c74862f5d2b54f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
