3c612b18521088baa6851bea5ca1528c387e4cb3
Antenor1998 <AntenorE18@hotmail.com>
2024-11-06 19:42:26 -0600

    first commit

--- src/Services/TenantService/TenantService.API/obj/Debug/net8.0/TenantService.API.AssemblyInfo.cs
+++ src/Services/TenantService/TenantService.API/obj/Debug/net8.0/TenantService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TenantService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b73a2ae22da97d97b891a6cbeb52a049535fb8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TenantService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TenantService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
