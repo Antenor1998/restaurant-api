9650fa7959c26846c026728d6c74862f5d2b54f5
Antenor1998 <AntenorE18@hotmail.com>
2024-11-06 20:09:49 -0600

    Add ocelot

--- src/Services/AuthService/AuthService.API/obj/Debug/net8.0/AuthService.API.AssemblyInfo.cs
+++ src/Services/AuthService/AuthService.API/obj/Debug/net8.0/AuthService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9f82989e892bd3f1247a88a6655b8ba87424b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5115a307fbad9076e6468105e34fac68424f492")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
