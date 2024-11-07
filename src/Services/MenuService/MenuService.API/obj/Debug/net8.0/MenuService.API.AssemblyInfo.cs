e5115a307fbad9076e6468105e34fac68424f492
Antenor1998 <AntenorE18@hotmail.com>
2024-11-06 19:45:38 -0600

    first commit

--- src/Services/MenuService/MenuService.API/obj/Debug/net8.0/MenuService.API.AssemblyInfo.cs
+++ src/Services/MenuService/MenuService.API/obj/Debug/net8.0/MenuService.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MenuService.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c612b18521088baa6851bea5ca1528c387e4cb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9f82989e892bd3f1247a88a6655b8ba87424b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MenuService.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MenuService.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
